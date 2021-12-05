17c437d65435ffdd7b787a1b1ef08502d117cf21
stam-721 <nao721.mutsu623@gmail.com>
2021-12-05 14:40:33 +0900

    課題「2-8」

---
+++ PHP/2-8/index.php
@@ -0,0 +1,10 @@
+<?php
+$fruits = ["appleといったらりんご"=>"","orangeといったらみかん"=>"","peachといったらもも"=>""];
+
+foreach ($fruits as $key=>$value) {
+    echo $key;
+    echo "<br>";
+    echo $value;
+   
+    
+}
