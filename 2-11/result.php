7c5e2b84c9c08b87b8c5c5f6a9eda215a035f60d
stam-721 <nao721.mutsu623@gmail.com>
2021-12-10 15:40:11 +0900

    課題「2-11」

---
+++ 2-11/result.php
@@ -0,0 +1,8 @@
+<?php
+$my_name = $_POST['my_name'];
+$password = $_POST['password'];
+$Email = $_POST['Email'];
+?>
+<p>私の名前は、<?php echo $my_name; ?>阿部</p>
+<p>私のパスワードは、<?php echo $Email; ?>○○@gmail.com</p>
+<p>私のパスワードは、<?php echo $password; ?>1111</p>
