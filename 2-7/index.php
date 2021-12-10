3d3860f105cb1d2e9170a1db7da2c4aee9f2cc2e
stam-721 <nao721.mutsu623@gmail.com>
2021-12-10 12:58:46 +0900

    課題「2-7」

--- 2-7/index.php
+++ 2-7/index.php
@@ -1,7 +1,10 @@
 <?php
 $color = ["red"=>"赤","blue"=>"青","green"=>"緑"];
 var_dump($color);
-echo "<br>";
-$color = ["red"=>"赤","blue"=>"青","green"=>"緑","yellow"=>"黄色"];
+
+ echo "<br/>";
+ $color = ["red"=>"赤","blue"=>"青","green"=>"緑"];
+$color["yellow"]="黄色";
+
 var_dump($color);
 ?>
