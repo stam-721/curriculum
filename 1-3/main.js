9b13a3d3f66ed08866486558156a0ff54b2042d1
stam-721 <nao721.mutsu623@gmail.com>
2021-12-23 10:54:00 +0900

    課題「1-3」

---
+++ 1-3/main.js
@@ -0,0 +1,26 @@
+// 問1
+let a =! 10;
+let b = 15;
+if (b >= 10 && b <= 20 && b % 2 === 0) {
+    console.log(b);
+}
+
+
+
+
+// 問2
+let x = 19;
+if (x >= 10 && x <= 20) {
+    console.log('成功です');
+} else {
+    console.log('失敗です')
+}
+
+
+// 問3
+let y = 0;
+if (y % 2 === 0) {
+    console.log('偶数です');
+}  if (y % 2 !== 0) {
+        console.log('奇数です');
+}
