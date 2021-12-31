ecc8a6b8a22c6ff8c85ebbe3806556e0afb88ce0
stam-721 <nao721.mutsu623@gmail.com>
2021-12-31 16:00:39 +0900

    課題「1-6」

---
+++ 1-6/main.js
@@ -0,0 +1,13 @@
+let scores = [5, 10, 15, 20, 25];
+console.log(scores);
+console.log('10は偶数です');
+console.log('20は偶数です');
+
+    
+let car = {
+    gass : 20.5,
+    num : 1234,
+};
+console.log(car);
+console.log('ガソリンは20.5です');
+console.log('ナンバーは1234です');
